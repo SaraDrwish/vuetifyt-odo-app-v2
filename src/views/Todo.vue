1ad0e28f0172e8a1102eeb2bbbb67055fd9cdc87
SaraDrwish <38770404+SaraDrwish@users.noreply.github.com>
2024-04-21 02:42:58 +0300

    7 lst

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,43 +1,63 @@
 <template>
   <div class="todo pa-2">
-    <div v-for="ts in tasks" :key="ts.id">
-      <v-list flat class="text-capitalize pa-0 ma-0">
-        <v-list-item
-          :class="{ 'green lighten-5': ts.done }"
-          @click="doneTask(ts.id)"
+    <v-container>
+      <div class="">
+        <v-text-field
+          @click:append="addTask"
+          @keyup.enter="addTask"
+          v-model="newTask"
+          outlined
+          color="orange"
+          label="Add Task"
+          append-icon="mdi-plus"
+          hide-details
+          clearable
         >
-          <!-- <template v-slot:default> -->
-          <v-list-item-action>
-            <v-checkbox :input-value="ts.done"></v-checkbox>
-          </v-list-item-action>
-          <!--
+        </v-text-field>
+      </div>
+
+      <!-- //// -->
+
+      <div v-for="ts in tasks" :key="ts.id">
+        <v-list flat class="text-capitalize pa-0 ma-0">
+          <v-list-item
+            :class="{ 'green lighten-5': ts.done }"
+            @click="doneTask(ts.id)"
+          >
+            <template v-slot:default>
+              <v-list-item-action>
+                <v-checkbox :input-value="ts.done"></v-checkbox>
+              </v-list-item-action>
+              <!--
             :class="
               ts.done == true ? 'text-decoration-line-through' : 'red--text' "-->
 
-          <!-- <v-list-item-content> -->
-          <v-list-item-title
-            :class="{ 'text-decoration-line-through': ts.done }"
-          >
-            {{ ts.title }}
-          </v-list-item-title>
-          <!-- </v-list-item-content> -->
+              <v-list-item-content>
+                <v-list-item-title
+                  :class="{ 'text-decoration-line-through': ts.done }"
+                >
+                  {{ ts.title }}
+                </v-list-item-title>
+              </v-list-item-content>
 
-          <v-list-item-action>
-            <v-btn icon>
-              <v-icon color="grey lighten-2">mdi-delete</v-icon>
-            </v-btn>
-          </v-list-item-action>
-          <!-- </template> -->
-        </v-list-item>
-      </v-list>
-      <v-divider></v-divider>
-    </div>
+              <v-list-item-action>
+                <v-btn icon @click="delTsk(ts.id)">
+                  <v-icon color="primary lighten-1">mdi-delete</v-icon>
+                </v-btn>
+              </v-list-item-action>
+            </template>
+          </v-list-item>
+        </v-list>
+        <v-divider></v-divider>
+      </div>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
   name: "Todo",
+  newTask: " ",
   data() {
     return {
       tasks: [
@@ -51,8 +71,21 @@ export default {
     doneTask(id) {
       const taskchosen = this.tasks.filter((el) => el.id === id)[0];
       taskchosen.done = !taskchosen.done;
-      console.log(taskchosen.done);
+      // console.log(taskchosen.done);
       // console.log("doneTask", id);
+    },
+    delTsk(id) {
+      this.tasks = this.tasks.filter((el) => el.id !== id);
+      // console.log("delTsk");
+    },
+    addTask() {
+      const newTaskAdded = {
+        id: Date.now(),
+        title: this.newTask,
+        done: false,
+      };
+      this.tasks.push(newTaskAdded);
+      this.newTask = "";
     },
   },
 };
